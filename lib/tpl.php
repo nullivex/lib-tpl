d3676383c92674b12b819b81e4001af76c609d63
Nullivex <contact@nullivex.com>
2013-02-25 02:16:15 -0800

    lots of changes for #78

--- lib/tpl.php
+++ lib/tpl.php
@@ -1,58 +1,55 @@
 <?php
-/*
- * YumZing
- * (c) 2010 EggPire LLC, All Rights Reserved.
- * Bryan Tong <contact@nullivex.com>
- */
+
+require_once(ROOT.'/usr/phptal/PHPTAL.php');
 
 class Tpl {
-	
-	const RETURN_BODY = true;
+
+	const OUTPUT_DIRECT = true;
 
 	static $inst = false;
 
-	protected $constants;
-	protected $lang;
-	protected $path;
-	protected $theme;
-	protected $body;
-	protected $tpl;
-	protected $file_ext = '.tpl.php';
-	protected $holder = 'tpl';
-	protected $theme_path;
-	protected $debug = array();
-	protected $css = '';
-	protected $js = '';
+	public $path = 'theme';
+	public $uri = '';
+	public $body = null;
+	public $init = false;
 
-	protected function __construct(){
-		//Void
-	}
+	protected $constants = array();
+	protected $debug = array();
+	protected $css = null;
+	protected $js = null;
 
 	public static function _get(){
-		if(self::$inst === false) self::$inst = new Tpl();
+		if(self::$inst === false){
+			$class = __CLASS__;
+			self::$inst = new $class();
+		}
 		return self::$inst;
 	}
-	
+
 	public function addCss($file,$media='screen'){
-		$css = "\t".'<link type="text/css" rel="stylesheet" href="'.$file.'" media="'.$media.'" />'."\n";
-		$this->css .= $css;
-		return $css;
+		return $this->css .= sprintf(
+			 "\t<link type=\"text/css\" rel=\"stylesheet\" href=\"%s\" media=\"%s\" />\n"
+			,$file
+			,$media
+		);
 	}
-	
+
 	public function addJs($file){
-		$js = "\t".'<script type="text/javascript" src="'.$file.'"></script>'."\n";
-		$this->js .= $js;
-		return $js;
+		return $this->js .= sprintf("\t<script type=\"text/javascript\" src=\"%s\"></script>\n",$file);
 	}
 
 	public function setPath($value){
 		$this->path = $value;
 		return $this;
 	}
-
-	public function setThemePath($path){
-		$this->theme_path = $path;
+	
+	public function setUri($value){
+		$this->uri = $value;
 		return $this;
+	}
+	
+	public function set($name,$value,$overwrite=true){
+		$this->setConstant($name,$value,$overwrite);
 	}
 
 	public function setConstant($name,$value,$overwrite=true){
@@ -61,9 +58,10 @@ class Tpl {
 		return true;
 	}
 
-	public function setLang($name,$value){
-		$name = "lang_".$name;
-		$this->lang[$name] = $value;
+	public function setConstants($constants=array(),$overwrite=true){
+		if(!is_array($constants)) return $this;
+		foreach($constants AS $name => $value) $this->setConstant($name,$value,$overwrite);
+		return $this;
 	}
 
 	public function addDebug($value){
@@ -75,142 +73,93 @@ class Tpl {
 		return $this;
 	}
 
-	public function setConstants($constants=array(),$overwrite=true){
-		if(!is_array($constants)) return $this;
-		foreach($constants AS $name => $value) $this->setConstant($name,$value,$overwrite);
-		return $this;
-	}
-
-	public function getConstant($name){
-		if(!isset($this->constants[$name])) return false;
+	public function get($name){
+		if(is_null($name)) return $this->constants;
+		if(!isset($this->constants[$name])) return null;
 		return $this->constants[$name];
 	}
 
-	public function getConstants(){
-		return $this->constants;
+	public function add($html){
+		return $this->body .= $html;
 	}
 
-	public function getLang($name){
-		$names = "lang_".$name;
-		if(!isset($this->lang[$name])) return false;
-		return $this->lang[$name];
-	}
-
-	public function addToBody($html){
-		$this->body .= $html;
-	}
-
-	public function resetBody(){
+	public function reset(){
 		$this->body = '';
-	}
-
-	public function parse($file,$section,$tags=array(),$return=false){
-		$this->load_file($file);
-		if(!isset($this->tpl[$file][$section])) return false;
-		$data = $this->parse_raw($this->tpl[$file][$section],$tags,$return);
-		return (($return) ? $data : $this);
-	}
-
-	public function parse_raw($data='',$tags=array(),$return=false){
-		//Replace Tags
-		if(is_array($tags)){
-			foreach($tags AS $tag => $value){
-				$tag = strval($tag);
-				if(empty($tag)) continue;
-				$value = strval($value);
-				$data = str_ireplace('{'.$tag.'}',$value,$data);
-			}
-		}
-		if($return) return $data;
-		$this->body .= $data;
 		return $this;
 	}
 
-	protected function load_file($file){
-		if(isset($this->tpl[$file])) return;
-		include($this->path.'/'.$file.$this->file_ext);
-		$holder = $this->holder;
-		if(!isset($$holder)) $this->tpl[$file] = array();
-		else $this->tpl[$file] = $$holder;
-		unset($tpl);
-	}
-
-	protected function parseConstants(){
-		$this->setConstants(Url::_all());
-		if(!is_array($this->constants)) return false;
-		foreach($this->constants AS $tag => $value){
-			$tag = strval($tag);
-			if(empty($tag)) continue;
-			$value = strval($value);
-			$this->body = str_ireplace('{'.$tag.'}',$value,$this->body);
-		}
-	}
-
-	public function initConstants(){
-
-		//Template Constants
-		$this->setConstant('site_name',Config::get('info','site_name'));
-		$this->setConstant('uri',Config::get('url','uri'));
-		$this->setConstant('url',Config::get('url','url'));
-		$this->setConstant('theme',$this->theme_path);
-		$this->setConstant('cur_year',date('Y'));
-		$this->setConstant('css',$this->theme_path.'/css');
-		$this->setConstant('js',Config::get('url','uri').'/js');
-		$this->setConstant('img',$this->theme_path.'/img');
-		$this->setConstant('alert','');
-		$this->setConstant('debug','');
-
-		//set delayed alerts
-		if(session('delayed_alert')){
-			$this->setConstant('alert',$this->getConstant('alert').session('delayed_alert'));
-			session('delayed_alert','');
-		}
-
-	}
-
 	public function stats(){
-		$end = microtime(true);
-		$time = number_format(($end - START),5);
-		$this->setConstant('script_stats',
-			'Execution: '.$time.' | Queries: '.Db::_get()->getQueryCount().
-			' | Memory: '.number_format((memory_get_usage()/1024/1024),2).'MB'
-		);
+		$stats = 'Execution: '.number_format((microtime(true) - START),5);
+		if(is_callable(array('Db','getQueryCount')))
+			$stats .= ' | Queries: '.Db::_get()->getQueryCount();
+		$stats .= ' | Memory: '.number_format((memory_get_usage()/1024/1024),2).'MB';
+		return $stats;
 	}
-	
+
 	public function debug(){
 		$dbg = array();
 		if(count($this->debug)){
-			$dbg[] = $this->parse('global','debug_init',array(),true);
 			foreach($this->debug as $d){
 				$title = sprintf('%s() @ line %d: %s',$d[0],$d[1],$d[2]);
-				$dbg[] = $this->parse('global','debug_entry',array('title'=>$title,'entry'=>$d[3],'handle'=>'dbg_'.md5($title.$d[3])),true);
+				$dbg[] = array('title'=>$title,'entry'=>$d[3],'handle'=>'dbg_'.md5($title.$d[3]));
 			}
-			$dbg[] = $this->parse('global','debug_end',array(),true);
 		}
-		$this->setConstant('debug',implode(PHP_EOL,$dbg));
-		unset($dbg);
-	}
-	
-	public function css(){
-		$this->setConstant('css',$this->css);
-	}
-	
-	public function js(){
-		$this->setConstant('js',$this->js);
+		return $dbg;
 	}
 
-	public function output(){
-		$this->css();
-		$this->js();
-		$this->stats();
-		$this->debug();
-		$this->parseConstants();
-		$this->parseConstants(); //2nd pass for const in const
-		$body = $this->body;
-		$this->resetBody();
-		return trim($body);
+	public function output($file,$tags=array(),$echo=false){
+		//if there is anything in the buffer, move it to debug
+		if(($content = ob_get_contents()) !== '')
+			$this->addDebug($content);
+		//init based on the theme (only once)
+		if(file_exists($this->path.'/init.php') && !$this->init){
+			include($this->path.'/init.php');
+			$this->init = true;
+		}
+		//init template handler
+		$tpl = new PHPTAL($this->path.'/'.$file);
+		//setup globals
+		if(!empty($this->css)) $this->setConstant('css',$this->css);
+		if(!empty($this->js)) $this->setConstant('js',$this->js);
+		//stats
+		$stats = $this->stats();
+		if(!empty($stats)) $this->setConstant('stats',$stats);
+		unset($stats);
+		//debug
+		$debug = $this->debug();
+		if(!empty($debug)) $this->setConstant('debug',$debug);
+		unset($debug);
+		//export globals to templating engine
+		$tpl->global = $this->constants;
+		//add global urls if the lib is loaded
+		if(is_callable(array('Url','_all')))
+			$tpl->urls = Url::_all();
+		//add tags to context
+		foreach($tags as $name => $val){
+			//dont add invalid vars
+			if(strpos($name,'_') === 0) continue;
+			if(strpos($name,' ') !== false) continue;
+			$tpl->$name = $val;
+		}
+		//execute template call
+		$out = $tpl->execute();
+		//if we have the tidy extension lets clean up the output
+		if(!extension_loaded('tidy')) return $out;
+		//cleanup the output
+		$tidy = new tidy();
+		$tidy->parseString($out,Config::get('theme','tidy'),'utf8');
+		$tidy->cleanRepair();
+		if($echo){
+			ob_end_clean();
+			echo $tidy;
+			return true;
+		} else return $tidy;
 	}
-	
+
+	public function __toString(){
+		return $this->output();
+	}
+
 }
 
 
