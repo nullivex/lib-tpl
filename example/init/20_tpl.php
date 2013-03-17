9bb8f296f78aae799614f036315b19a5f2e8acb7
Tony Butler <tony@esited.com>
2013-03-17 12:37:01 -0700

    why not a couple more Config changes

--- example/init/20_tpl.php
+++ example/init/20_tpl.php
@@ -8,12 +8,12 @@ Tpl::_get()->setUri('/theme/'.$theme.'/');
 Tpl::_get()->set(array(
 	 'lss_version'		=>	LSS_VERSION
 	,'version'			=>	VERSION
-	,'site_name'		=>	Config::get('info','site_name')
-	,'site_title'		=>	Config::get('info','site_name')
+	,'site_name'		=>	Config::get('site_name')
+	,'site_title'		=>	Config::get('site_name')
 	,'uri'				=>	Config::get('url','uri')
 	,'url'				=>	Config::get('url','url')
 	,'theme_path'		=>	Tpl::_get()->uri
-	,'copyright'		=>	'© '.date('Y').' '.Config::get('info','site_name')
+	,'copyright'		=>	'© '.date('Y').' '.Config::get('site_name')
 ));
 
 //set delayed alerts
